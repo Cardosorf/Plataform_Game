4be0339ae7f7a679f75c04cb689bfed9a9a1b112
Rafael Cardoso <cardoso.rf@gmail.com>
2019-07-21 01:26:41 -0300

    +Character Controller, Custom Pallet, Random Tile, Rule Tile
    Added Character Controller
    Added Custom Pallet.
    Added Random Tile (grass/flower generator).
    Added Rule Tile (watterfall).

---
+++ Platform Game/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+
+
+    public CharacterController2D controller;
+
+    public float horizontalMover = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+    }
+}
