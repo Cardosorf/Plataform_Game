e4b4f0729dac9f0714eb0fea99047c56c3b11107
Rafael Cardoso <cardoso.rf@gmail.com>
2019-07-22 18:34:56 -0300

    +Jump, Water RuleTile, Tags, player animation; #Water Tilemap
    Some adjustments on Water Tilemap.
    Added jump.
    Added Water Rule Tile.
    Added Ground and Player tag.
    Added player idle animation.
    Added player jumple animation.
    Added player run animation.

--- Platform Game/Assets/PlayerController.cs
+++ Platform Game/Assets/PlayerController.cs
@@ -7,15 +7,23 @@ public class PlayerController : MonoBehaviour
 
 
     public float runSpeed = 400;
+    public float jumpForce = 600;
+
+    private bool facingRight = true;
+    private bool isJumping = false;
 
     private Rigidbody2D rb;
 
-    private bool facingRight = true;
+    private Animator animator;
+
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,11 +38,40 @@ public class PlayerController : MonoBehaviour
 
         float move = Input.GetAxisRaw("Horizontal");
 
-        
+        animator.SetFloat("Speed", Mathf.Abs(move));
 
         rb.velocity = new Vector2(runSpeed * move * Time.fixedDeltaTime, rb.velocity.y);
 
         Flip(move);
+
+        Jump();
+
+        
+
+        if(rb.velocity.y == 0f)
+        {
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isFalling", false);
+        }
+
+        if (rb.velocity.y > 0f)
+        {
+            animator.SetBool("isJumping", true);
+            animator.SetBool("isFalling", false);
+            Debug.Log("Jumping");
+            Debug.Log(rb.velocity.y);
+
+        }
+
+        if (rb.velocity.y < 0f)
+        {
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isFalling", true);
+            Debug.Log("Falling");
+            Debug.Log(rb.velocity.y);
+        }
+
+
         //transform.Translate(Input.GetAxisRaw("Horizontal") * runSpeed * Time.fixedDeltaTime,0f,0f);
 
     }
@@ -53,6 +90,45 @@ public class PlayerController : MonoBehaviour
         }
 
 
+    }
+
+    private void Jump()
+    {
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            //animator.SetBool("isJumping", true);
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+
+            rb.velocity = Vector2.zero;
+            //rb.angularVelocity = 0f;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        //Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+
+        }
     }
 
 }
